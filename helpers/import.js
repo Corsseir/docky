8d60e514761b4cc50b211e9fb703f1ff8b6782e9
Piotr Maciejko <corsseir@gmail.com>
2016-10-21 18:42:14 +0200

    first commit

---
+++ helpers/import.js
@@ -0,0 +1,19 @@
+/**
+ * Created by corsseir on 10/21/16.
+ */
+
+function Import () {
+    var getTemplate = function (linkId, sectionId, targetId) {
+        var link = document.querySelector(linkId);
+        var template = link.import.querySelector(sectionId);
+        var clone = document.importNode(template.content, true);
+
+        document.querySelector(targetId).appendChild(clone);
+    }
+
+    return {
+        getTemplate: getTemplate,
+    }
+}
+
+module.exports = {Import};
---
+++ renderer.js
@@ -0,0 +1,56 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+function Start () {
+    var init = function (event) {
+        new Import().getTemplate('#link-section-default', '#section-default', '#side-right');
+    }
+    init();
+}
+
+function Folder () {
+    var handleFolderClick = function (event) {
+        var folder = $(event.target);
+        var folderIcon = folder.find('i');
+        var folderList = folder.closest('li').find('ul').first();
+
+        if(folderIcon.hasClass('fa-folder')) {
+            folderIcon.removeClass('fa-folder');
+            folderIcon.addClass('fa-folder-open');
+
+            if(typeof(folderList) !== 'undefined') {
+                folderList.show();
+            }
+        } else if(folderIcon.hasClass('fa-folder-open')) {
+            folderIcon.removeClass('fa-folder-open');
+            folderIcon.addClass('fa-folder');
+
+            if(typeof(folderList) !== 'undefined') {
+                folderList.hide();
+            }
+        }
+    }
+
+    var init = function () {
+        $(document).on('click', '.folder', handleFolderClick);
+    }
+    init();
+}
+
+function File () {
+    var handleFileClick = function (event) {
+        $(document).find('#side-right').first().empty();
+        new Import().getTemplate('#link-section-info', '#section-info', '#side-right');
+    }
+
+    var init = function () {
+        $(document).on('click', '.file', handleFileClick);
+    }
+    init();
+}
+
+new Start();
+new Folder();
+new File();
+
