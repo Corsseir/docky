3ae5a7fcc8117c10a67240b0750ceeae30abee22
mike <michamalcahn@gmail.com>
2016-11-04 20:24:13 +0100

    Dodałem index z tymczasowym plikiem.

--- scanDirs.js
+++ scanDirs.js
@@ -2,10 +2,16 @@
  * Created by mike on 11/3/16.
  */
 //Rozpoczyna skanowanie folderów w celu znalezienia plików pdf po wcisnięciu elementu gui o id = "scan"
-    //Nie chcę robić sufu w indexie, więc trzeba dorzucić przycisk gdzieś, żeby zadziałało.
+//Następnie tworzy katalog na pliki pdf
 const {dialog} = require('electron').remote
 const fs = require ('fs')
-path = require("path")
+const libraryPath = "./DockyLibrary"
+const overwritePath = "./DockyLibrary/Overwrite"
+const util = require('util')
+let streamEqual = require('stream-equal')
+let path = require("path")
+
+
 function scanDirs () {
     var rootPath = dialog.showOpenDialog({properties:['openDirectory']})
     var dirs = []
@@ -13,6 +19,9 @@ function scanDirs () {
     dirs.push(rootPath.toString())
     listPDFs(dirs[0], dirs, pdfs)
     pdfs.forEach(function(element) {console.log("Ścieżka do pdfa: " + element)})
+    createDir(libraryPath)
+    createDir(overwritePath)
+    createLib(pdfs)
     return pdfs
 }
 
@@ -26,7 +35,7 @@ function listPDFs (dirpath, dirsArray, filesArray) {
         }
         if (fs.lstatSync(filepath).isFile()) {
             if (path.extname(filepath) === ".pdf") {
-                filesArray.push(filepath)
+                filesArray.push(filepath.toString())
             }
         }
     }
@@ -39,6 +48,59 @@ function listPDFs (dirpath, dirsArray, filesArray) {
     //Przejdź rekurencyjnie po wszystkich podfolderach.
     for (var i = 0; i < dirsArray.length; i++) {
         listPDFs(dirsArray[i], dirsArray, filesArray)
+    }
+}
+
+function createDir (path) {
+    try {
+        var stats = fs.lstatSync(path)
+        console.log("Katalog już istnieje")
+    }
+    catch (e) {
+        if (e.code === 'ENOENT'){
+            fs.mkdirSync(path)
+            console.log("Stworzono katalog")
+        }
+        else {
+            throw e
+        }
+    }
+}
+
+function createLib (pdfArray) {
+    for (var i = 0; i < pdfArray.length; i++) {
+        var fName = path.basename(pdfArray[i]).toString()
+        var pathInLib = libraryPath + "/" + fName
+        var filePath = pdfArray[i]
+        addFile(pathInLib, filePath)
+    }
+}
+
+function addFile (pathInLib, filePath) {
+    try {
+        fs.accessSync(pathInLib, fs.F_OK)
+        console.log("plik: " + pathInLib + " istnieje")
+        var file1 = fs.createReadStream(pathInLib.toString())
+        var file2 = fs.createReadStream(filePath.toString())
+        streamEqual(file1, file2, function (err, equal) {
+            //zapisz plik do folderu overwrite
+            if (!equal) {
+                var oldName = path.basename(pathInLib, '.pdf').toString()
+                //Kazdy powtarzający sie plik bedzie znajdowac sie w katalaogu, ktory nazywa sie jak ten plik
+                var originDirName = overwritePath + "/" + path.basename(filePath, ".pdf").toString()
+                createDir(originDirName)
+                var newName = originDirName + "/" + oldName + "_new" + ".pdf"
+                addFile(newName, filePath)
+                console.log("Wpisalem " + newName + " do overwrite")
+            }
+        })
+    } catch (e) {
+        if (e.code === 'ENOENT') {
+            console.log("Zapisuje nie istniejący wczesniej plik: " + path.basename(filePath))
+            fs.createReadStream(filePath).pipe(fs.createWriteStream(pathInLib))
+        } else {
+            throw e
+        }
     }
 }
 
