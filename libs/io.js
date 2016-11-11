bdacd1f3f91f25c428c4c1d21e4d3e39d5b4914a
mike <michamalcahn@gmail.com>
2016-11-11 17:42:52 +0100

    Simplified addF -> now use addFile

--- libs/io.js
+++ libs/io.js
@@ -31,7 +31,14 @@ class IO {
     static scan() {
         var rootPath = dialog.showOpenDialog({properties: ['openDirectory']})
         var pdfs = this.scanDirs(rootPath)
-
+        console.log("Znalazłem: " + pdfs.length + " pdfów")
+        for (var i = 0; i < pdfs.length; i++) {
+            if (pdfs[i].includes(libraryPath)) {
+                pdfs.splice(i, 1)
+            } else {
+                console.log("PDF: " + pdfs[i])
+            }
+        }
         return pdfs
     }
 
@@ -43,11 +50,11 @@ class IO {
 
         for (i = 0; i < pdfs.length; i++) {
             var element = pdfs[i]
-            console.log(element)
-            console.log(collectionId)
+            console.log("addToLibAndDb Element: " + element + "Collection id: " + collectionId)
             var fName = path.basename(element).toString()
             var pathInLib = libraryPath + "/" + fName
-            self.addF(pathInLib, element, function (result) {
+            self.addFile(pathInLib, element, function (result) {
+                console.log("Obiekt result: " + result.local + " , " + result.type)
                 self.addToDb(result, collectionId, filename, function () {
                     if(i === pdfs.length && typeof filename !== 'undefined') {
                         callback()
@@ -55,19 +62,23 @@ class IO {
                 })
             })
         }
+
+        if (i === pdfs.length - 1) {
+            console.log("Zakonczono dodawanie")
+        }
     }
 
     static addToLibAndDbFromScan(pdfs, callback) {
         var self = this;
 
         DatabaseOperation.Collection.GetAllCollections(function (err, rows) {
-            var rootCollection = CollectionHelper.getRootCollection(rows)
+                var rootCollection = CollectionHelper.getRootCollection(rows)
 
-            DatabaseOperation.Collection.CreateCollection("Zeskanowane", rootCollection.ID_Collection, function () {
-                var collectionId = this.lastID
+                DatabaseOperation.Collection.CreateCollection("Zeskanowane", rootCollection.ID_Collection, function () {
+                    var collectionId = this.lastID
 
-                self.addToLibAndDb(pdfs, collectionId)
-            })
+                    self.addToLibAndDb(pdfs, collectionId)
+                })
         })
     }
 
@@ -77,12 +88,11 @@ class IO {
         console.log("Jestem w add to DB")
         var fileLocalPath = element.local
         var fileSysPath = element.filesys
-        console.log(fileName)
+        //console.log("Dodaje do db plik : " + fileName)
         if(typeof fileName === 'undefined') {
             fileName = path.basename(element.filesys).toString().split('.')[0]
         }
-        console.log(fileName)
-        console.log(collectionId)
+        console.log("addToDb: collection id: " + collectionId + " file name: " + fileName)
 
         this.addEntryToDb(fileName, fileLocalPath, fileSysPath, collectionId, function () {
             console.log('Dzialam2')
@@ -95,12 +105,12 @@ class IO {
     static createDir(path) {
         try {
             var stats = fs.lstatSync(path)
-            console.log("Katalog już istnieje")
+            console.log("Katalog " + path + " już istnieje")
         }
         catch (e) {
             if (e.code === 'ENOENT') {
                 fs.mkdirSync(path)
-                console.log("Stworzono katalog")
+                console.log("Stworzono katalog: " + path)
             }
             else {
                 throw e
@@ -126,19 +136,19 @@ class IO {
     static addEntryToDb(fileName, fileLocalPath, fileSysPath, collectionId, callback) {
             DatabaseOperation.File.CreateFile(null, fileName, null, function addFileId() {
                 var fileId = this.lastID
-                console.log("Jestem w create file " + fileId)
+                console.log("Jestem w create file " + "F_ID: " + fileId + " Sciezka: " + fileLocalPath)
                 DatabaseOperation.Location.CreateLocation("local", fileLocalPath, function addLocationId() {
                     var locationId = this.lastID
-                    console.log("Jestem w create location " + locationId + " test file: " + fileId)
+                    console.log("Jestem w create location typ:local " + "L_ID:" + locationId + " FileID: " + fileId)
                     DatabaseOperation.File_Location.CreateFile_Location(fileId, locationId, function () {
-                        console.log("Skonczylem dodawać lokalną")
+                        console.log("Skonczylem dodawać do F_L lokalną sciezkę")
                     })
                 })
                 DatabaseOperation.Location.CreateLocation("global", fileSysPath, function addLocationId() {
                     var locationId2 = this.lastID
-                    console.log("Jestem w create location " + locationId2 + " test file: " + fileId)
+                    console.log("Jestem w create location typ:global " + "L_ID: " + locationId2 + " F_ID: " + fileId)
                     DatabaseOperation.File_Location.CreateFile_Location(fileId, locationId2, function () {
-                        console.log("Skonczylem dodawać globalną")
+                        console.log("Skonczylem dodawać do F_L globalną scieżkę")
                     })
                 })
                 console.log('Dzialam1')
@@ -192,14 +202,14 @@ class IO {
     static addF(pathInLib, filePath, callback) {
         var result = new Object()
         if (fs.existsSync(pathInLib)) {
-            console.log(pathInLib + " już jest")
+            console.log(pathInLib + " już jest w bibliotece")
             var baseN = path.basename(filePath, ".pdf").toString()
             var ovPath = overwritePath + "/" + baseN
             fs.mkdir(ovPath, function handleCreated(err) {
                 if (err) {
                     if (err.code === "EEXIST") {
                         fs.readdir(ovPath, function getFolders(err, files) {
-                            console.log("Katalog w override juz jest")
+                            console.log("Katalog o nazwie" + ovPath + "jest już utworzony w overwrite")
                             var x = 0
                             var max = 0
                             for (var i = 0; i < files.length; i++) {
@@ -219,6 +229,7 @@ class IO {
                         })
                     }
                 } else {
+                    console.log("Katalog o nazwie" + ovPath + "zostanie utworzony w overwrite")
                     var newPath = ovPath + "/" + baseN + "__1" + ".pdf"
                     fs.createReadStream(filePath).pipe(fs.createWriteStream(newPath))
                     result.filesys = filePath
@@ -228,13 +239,78 @@ class IO {
                 }
             })
         } else {
-            console.log(pathInLib + " nie ma")
+            console.log("Pliku o sciezce: " + pathInLib + " nie ma w bibliotece. Kopiowanie rozp.")
             fs.createReadStream(filePath).pipe(fs.createWriteStream(pathInLib))
             console.log(1)
             result.filesys = filePath
             result.local = pathInLib
             result.type = "unique"
             callback && callback(result)
+        }
+    }
+
+// Uporządkowane addF, ponizej sama funkcja i funkcje jej dotycące
+    static createResult(resultObj, sysPath, libPath, type, callback) {
+        resultObj.filesys = sysPath
+        resultObj.local = libPath
+        resultObj.type = type
+        callback && callback (resultObj)
+    }
+
+    static createOvFilePath(ovFolderPath, baseN, files, callback) {
+        var x = 0
+        var max = 0
+        for (var i = 0; i < files.length; i++) {
+            var oldNumber = (parseInt(files[i].split("__").pop()))
+            if (max < oldNumber) {
+                max = oldNumber
+            }
+        }
+        var newNumber = (max + 1).toString()
+        var newName = "__" + newNumber
+        var newPath = ovFolderPath + "/" + baseN + newName + ".pdf"
+        callback && callback(newPath)
+    }
+
+    static addToOverwrite(err, self, result, filePath, ovPath, baseN, callback){
+        if (err) {
+            if (err.code === "EEXIST") {
+                fs.readdir(ovPath, function getFolders(err, files) {
+                    console.log("Katalog o nazwie" + ovPath + "jest już utworzony w overwrite")
+                    self.createOvFilePath(ovPath, baseN, files, function (newPath){
+                        fs.createReadStream(filePath).pipe(fs.createWriteStream(newPath))
+                        self.createResult(result, filePath, newPath, "overwrite", function sendResult(resultObj) {
+                            callback && callback(resultObj)
+                        })
+                    })
+                })
+            }
+        } else {
+            console.log("Katalog o nazwie" + ovPath + "zostanie utworzony w overwrite")
+            var newPath = ovPath + "/" + baseN + "__1" + ".pdf"
+            fs.createReadStream(filePath).pipe(fs.createWriteStream(newPath))
+            self.createResult(result, filePath, newPath, "overwrite", function sendResult(resultObj) {
+                callback && callback(resultObj)
+            })
+        }
+    }
+    static addFile(pathInLib, filePath, callback){
+        var result = new Object()
+        var self = this
+        if (fs.existsSync(pathInLib)) {
+            console.log(pathInLib + " już jest w bibliotece")
+            var baseN = path.basename(filePath, ".pdf").toString()
+            var ovPath = overwritePath + "/" + baseN
+            fs.mkdir(ovPath, function handleMk(err) {
+                self.addToOverwrite(err, self, result, filePath, ovPath, baseN, callback)
+            })
+        } else {
+            console.log("Pliku o sciezce: " + pathInLib + " nie ma w bibliotece. Kopiowanie rozp.")
+            fs.createReadStream(filePath).pipe(fs.createWriteStream(pathInLib))
+            console.log("Liczba Piotra: " + 1)
+            self.createResult(result, filePath, pathInLib, "unique", function sendResult(resultObj) {
+                callback && callback(resultObj)
+            })
         }
     }
 }
