2ecd9e60be17f9980a134ebabee299238c0fc90a
Piotr Maciejko <corsseir@gmail.com>
2016-12-11 17:04:25 +0100

    metadane

--- renderers/file.js
+++ renderers/file.js
@@ -5,30 +5,72 @@
 const {ipcRenderer} = require('electron')
 const {Tree} = require('../renderer.js')
 const {Dialog} = require('electron').remote.require('./helpers/dialog.js')
+const {PDFJS} = require('../libs/pdf.js')
 
 class File {
-    handleOpenClick(event) {
+    showMetaData(path) {
+        var notify = new Notification()
+
+        PDFJS.workerSrc = 'libs/pdf.worker.js'
+        PDFJS.getDocument(path).then(function (pdf) {
+            pdf.getMetadata().then(function (metadata) {
+                console.log(metadata)
+                if (typeof metadata !== 'undefined') {
+                    if (typeof metadata.info !== 'undefined') {
+                        $('#id_tag').val('')
+                        if (typeof metadata.info.Author !== 'undefined') {
+                            if(metadata.info.Author !== '')
+                            {
+                                $('#id_tag').val($('#id_tag').val() + 'author:' + metadata.info.Author.split(' ').join('_'))
+                            }
+                        }
+
+                        if (typeof metadata.info.Title !== 'undefined') {
+                            if(metadata.info.Title !== '') {
+                                if ($('#id_tag').val().length !== 0) {
+                                    $('#id_tag').val($('#id_tag').val() + ' ')
+                                }
+
+                                $('#id_tag').val($('#id_tag').val() + 'title:' + metadata.info.Title.split(' ').join('_'))
+                            }
+                        }
+                    }
+                }
+            })
+        }).catch(function(err) {
+            notify.show('Błędna ścieżka', 3000)
+        });
+    }
+
+    handleOpenClick(event, self) {
         var path = new Dialog().file()
 
         $('#id_path').val(path)
+        self.showMetaData(path[0])
+    }
+
+    handlePathKeyPress(event, self) {
+        if(event.which === 13) {
+            self.showMetaData($('#id_path').val())
+        }
     }
 
     check(tags) {
         var notify = new Notification()
         var result = true
 
-        if(tags != '') {
+        if (tags != '') {
             tags = tags.split(' ')
 
-            for(var i = 0; i < tags.length; i++) {
+            for (var i = 0; i < tags.length; i++) {
                 var tag = tags[i].split(':')
 
-                if(tag.length === 1 || tag.length > 2) {
+                if (tag.length === 1 || tag.length > 2) {
                     notify.show('Błędny zapis jednego ze słów kluczowych', 3000)
                     result = false
                     break
-                } else if(tag.length === 2) {
-                    if(tag[1] === '') {
+                } else if (tag.length === 2) {
+                    if (tag[1] === '') {
                         notify.show('Klucz musi posiadać wartość', 3000)
                         result = false
                         break
@@ -46,11 +88,11 @@ class File {
         var notify = new Notification()
         var isValid = self.check(data.tag)
 
-        if(isValid) {
-            if(data.path.length !== 0) {
+        if (isValid) {
+            if (data.path.length !== 0) {
                 var check = data.path.split('.')
                 var length = check.length
-                if(check[length - 1].indexOf('pdf') !== 0) {
+                if (check[length - 1].indexOf('pdf') !== 0) {
                     notify.show('Obsługiwane są tylko pliki PDF', 3000)
                 } else {
                     notify.show('Dodawanie...', 0, function () {
@@ -64,7 +106,7 @@ class File {
                                 notify.show('Wskazany plik nie istnieje', 3000)
                             })
                         } else if (typeof result.file !== 'undefined') {
-                            if(result.status === 'exist') {
+                            if (result.status === 'exist') {
                                 notify.hide(function () {
                                     notify.show('Plik istnieje już w bazie danych pod nazwą \'' + result.file.Filename + '\'', 3000)
                                 })
@@ -84,7 +126,7 @@ class File {
         var notify = new Notification()
         var isValid = self.check(data.tag)
 
-        if(isValid) {
+        if (isValid) {
             if (data.name.length !== 0) {
                 result = ipcRenderer.sendSync('editFile', {'data': data})
 
@@ -100,7 +142,12 @@ class File {
     constructor() {
         var self = this
 
-        $(document).on('click', '#id_path-icon', self.handleOpenClick)
+        $(document).on('click', '#id_path-icon', function (event) {
+            self.handleOpenClick(event, self)
+        })
+        $(document).on('keypress', '#id_path', function (event) {
+            self.handlePathKeyPress(event, self)
+        })
         $(document).on('click', '#accept-file-add', function (event) {
             self.handleAcceptFileAddClick(event, self)
         })
